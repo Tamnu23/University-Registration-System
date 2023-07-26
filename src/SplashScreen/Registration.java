23bf72222df6f855b8dc918f3cf47380857492dc
Htet Aung Shine <103210228+HtetAungShine6@users.noreply.github.com>
2023-07-26 15:18:36 +0700

    added user site registration and login

--- src/SplashScreen/Registration.java
+++ src/SplashScreen/Registration.java
@@ -5,10 +5,12 @@ import Student.StudentMain;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import Student.StudentRegister;
+import Instructor.InstructorRegister;
 
 public class Registration extends JFrame implements ActionListener{
     
-    JButton student,instructor;
+    JButton student,instructor, already;
     
     public Registration(){
         getContentPane().setBackground(Color.WHITE);
@@ -30,6 +32,9 @@ public class Registration extends JFrame implements ActionListener{
         instructor.setFont(new Font("Tahoma",Font.BOLD,15));
         add(instructor);
         
+        already = new JButton("Already have an account? Login Here");
+        already.setBounds(138, 600, 200, 60);
+        
         setSize(600,900);
         setLocation(500,250);
         setVisible(true);
@@ -37,9 +42,9 @@ public class Registration extends JFrame implements ActionListener{
     
     public void actionPerformed(ActionEvent ae){
         if(ae.getSource() == student){
-            new StudentMain();
+            new StudentRegister();
         }else if(ae.getSource() == instructor){
-            new InstructorMain();
+            new InstructorRegister();
         }
     }
     
---
+++ src/Student/StudentRegister.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Student;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import Connection.Conn;
+
+public class StudentRegister extends JFrame implements ActionListener{
+    JButton register, cancel;
+    JTextField tfname, tfemail;
+    JPasswordField tfpassword;
+    
+    public StudentRegister(){
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Register as Student");
+        heading.setBounds(170,20,500,50);
+        heading.setFont(new Font("serif", Font.BOLD,30));
+        add(heading);
+        
+        JLabel lblusername = new JLabel("Username");
+        lblusername.setBounds(40, 80, 100, 20);
+        add(lblusername);
+        
+        tfname = new JTextField();
+        tfname.setBounds(150,80,150,20);
+        add(tfname);
+        
+        JLabel lblemail = new JLabel("Email");
+        lblemail.setBounds(40,130,100,20);
+        add(lblemail);
+        
+        tfemail = new JTextField();
+        tfemail.setBounds(150, 130, 150, 20);
+        add(tfemail);
+        
+        JLabel lblpassword = new JLabel("Password");
+        lblpassword.setBounds(40, 190, 100, 20);
+        add(lblpassword);
+        
+        tfpassword = new JPasswordField();
+        tfpassword.setBounds(150, 190, 150, 20);
+        add(tfpassword);
+        
+        System.out.println();
+        
+        register = new JButton("Register");
+        register.setBounds(40, 250, 120, 30);
+        register.setBackground(Color.WHITE);
+        register.setForeground(Color.BLACK);
+        register.addActionListener(this);
+        register.setFont(new Font("Tahoma",Font.BOLD,15));
+        add(register);
+        
+        cancel = new JButton("Cancel");
+        cancel.setBounds(180, 250, 120, 30);
+        cancel.setBackground(Color.WHITE);
+        cancel.setForeground(Color.BLACK);
+        cancel.addActionListener(this);
+        cancel.setFont(new Font("Tahoma",Font.BOLD,15));
+        add(cancel);
+        
+        setSize(600,450);
+        setLocation(500,250);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource() == register){
+            String name = tfname.getText();
+            String email = tfemail.getText();
+            String password = tfpassword.getText();
+            
+            try{
+                String query = "insert into studentRegister values('"+name+"', '"+email+"', '"+password+"')";
+                Conn con = new Conn();
+                con.s.execute(query);
+                
+                JOptionPane.showMessageDialog(null, "Registered Successfully");
+                setVisible(false);
+                new StudentLogin();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }else{
+            setVisible(false);
+        }
+    }
+    
+    public static void main(String[] args){
+        new StudentRegister();
+    }
+}
